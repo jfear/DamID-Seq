9cbf04dee817e477a9648c7da11eab3a4fe338ef
Justin Fear <justin.fear@nih.gov>
2016-11-10 09:20:59 -0500

    Changes hard coded All_GATC_list.txt to be environment install.

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -5,7 +5,7 @@ if [ ! -e ${PREFIX}/share/DamID-Seq ]; then
     mkdir -p ${PREFIX}/share/DamID-Seq
 fi
 
-cp Perl_scripts/*.txt ${PREFIX}/share/DamID-Seq
+cp Perl_scripts/*.txt ${PREFIX}/share/DamID-Seq/
 
 # Copy scripts to the bin directory
 if [ ! -e ${PREFIX}/bin ]; then
@@ -13,4 +13,5 @@ if [ ! -e ${PREFIX}/bin ]; then
 fi
 
 cp Perl_scripts/*.pl ${PREFIX}/bin/
+LANG=C sed "s#All_GATC_list.txt#${PREFIX}/share/DamID-Seq/All_GATC_list.txt#" Perl_scripts/1_GATC_mapper.pl > ${PREFIX}/bin/1_GATC_mapper.pl 
 chmod a+x ${PREFIX}/bin/*.pl
